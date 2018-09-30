eb2050fb14982e2be737d64c636a5effff9c1798
cjan225 <cjan225@g.uky.edu>
2018-09-29 22:35:26 -0400

    Added MainMenu structure.

--- src/App.py
+++ src/App.py
@@ -1,7 +1,7 @@
 import sys
-from PyQt5.QtWidgets import *
-from PyQt5.QtGui import *
 from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+from PyQt5.QtWidgets import *
 from PyQt5 import uic
 
 from Table import *
@@ -12,6 +12,7 @@ class AppWindow(QMainWindow):
         super(AppWindow, self).__init__()
         self.initMainWindow()
         self.createPopupMenu()
+        self.initMainMenu()
         self.handleTableWidget()
         self.initUI()
 
@@ -20,12 +21,46 @@ class AppWindow(QMainWindow):
         self.setWindowTitle("Main Window")
         self.resize(1200,800)
 
+
+    def initMainMenu(self):
+        mBar = self.menuBar()
+        self.fileMenu = mBar.addMenu("File")    #Menu
+        self.fileMenu.addAction("New")          #Item of Submenu File
+        self.fileMenu.addAction("Open")         #Item of Submenu File
+        self.fileMenu.addAction("Export")       #Item of Submenu File
+        self.fileMenu.addAction("Quit")         #Item of Submenu File
+
+        self.editMenu = mBar.addMenu("Edit")    #Menu
+        self.editMenu.addAction("Cut")          #Item of Submenu Edit
+        self.editMenu.addAction("Copy")         #Item of Submenu Edit
+        self.editMenu.addAction("Paste")        #Item of Submenu Edit
+
+        self.viewMenu = mBar.addMenu("View")    #Menu
+        self.viewMenu.addAction("Semi-Auto")    #Item of Submenu View
+        self.viewMenu.addAction("Auto")         #Item of Submenu View
+
+        self.helpMenu = mBar.addMenu("Help")    #Menu
+        self.helpMenu.addAction("About")        #Item of Submenu Help
+
+        # save = QAction("Save", self)
+        # save.setShortcut("Ctrl+S")
+        # self.fileMenu.addAction(save)
+
+        #set Bindings from QActions to relevant functions
+        self.fileMenu.triggered[QAction].connect(self.fileTrigger)
+
+    #debug function for bindings
+    def fileTrigger(self, q):
+        print(" is triggered")
+
+    #Initalize/show ui components here
     def initUI(self):
-        self.setCentralWidget(self.mTable)
         self.show()
 
+    #handles TableWidget stuff
     def handleTableWidget(self):
         self.mTable = Table()
+        self.setCentralWidget(self.mTable)
 
     #def handleButtonWidget(self):
     #def handleVisionWidget(self):
