1d9e5b5e0a237d5ad1c295366d7e238b632bc624
Andrew Tapia <andrew.tapia@uky.edu>
2018-11-11 03:48:39 -0500

    Add rudimentary lap prediction function.
    Change Lap_Time to better suit prediction and testing.

--- src/system/Time.py
+++ src/system/Time.py
@@ -1,10 +1,22 @@
 from time import time
 
 class Lap_Time():
-    def __init__(self, previousTime = None):
-        self.recordedTime = time()
-        if previousTime is None:
-            self.elapsedTime = None
-        else:
-            self.elapsedTime = self.recordedTime - previousTime
+    def __init__(self, recordedTime, elapsedTime = None):
+        self.recordedTime = recordedTime
+        self.elapsedTime = elapsedTime
 
+
+    @classmethod
+    def fromCurrentTime(cls, previousTime = None):
+        recordedTime = time()
+        if previousTime is None:
+            elapsedTime = None
+        else:
+            elapsedTime = recordedTime - previousTime
+        return cls(recordedTime,elapsedTime)
+
+    # def __eq__(self, other):
+    #     return (self.recordedTime == other.recordedTime) and (self.elapsedTime == other.elapsedTime)
+
+    # def __ne__(self, other):
+    #     return not self == other
