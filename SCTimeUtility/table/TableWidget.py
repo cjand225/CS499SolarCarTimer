ab64b2283f0586ec0a75f4bd60992a17f9161c6a
Charles <cjan225@g.uky.edu>
2019-01-19 12:01:08 -0500

    updated documentation

--- SCTimeUtility/table/TableWidget.py
+++ SCTimeUtility/table/TableWidget.py
@@ -16,11 +16,29 @@ class TableWidget(QWidget):
         self.initUI()
         self.saveShortcut = QShortcut(QKeySequence("Ctrl+S"), self)
 
+    '''
+        Function: initUI
+        Parameters: self
+        Return Value: N/A
+        Purpose: Initializes the GUI for TableWidget by loading its' resource file, storing a reference to it 
+                 in self.UI
+                 
+    '''
+
     def initUI(self):
         self.ui = loadUi(self.uiPath, self)
         self.setGeometry(QStyle.alignedRect(Qt.LeftToRight, Qt.AlignCenter,
                                             self.size(), QApplication.desktop().availableGeometry()))
         self.show()
+
+    '''
+        Function: initHeaderHorizontal
+        Parameters: self
+        Return Value: N/A
+        Purpose: Initializes the horizontal headers for the table view widget, such that it has
+                 proper spacing for items its displaying
+
+    '''
 
     def initHeaderHorizontal(self):
         # Resizes the horizontal header so that the table fits initially without scrollbars.
@@ -31,6 +49,15 @@ class TableWidget(QWidget):
             self.tableView.horizontalHeader().sectionSize(headerIndex)
         self.tableView.horizontalHeader().setSectionResizeMode(QHeaderView.Interactive)
 
+    '''
+        Function: iniHeaderVertical
+        Parameters: self
+        Return Value: N/A
+        Purpose: Initializes the vertical headers for the table view widget, such that it has
+                 proper spacing for items its displaying
+
+    '''
+
     def initHeaderVertical(self):
         # Resizes the vertical header so that the table fits initially without scrollbars.
         self.tableView.verticalHeader().setSectionResizeMode(QHeaderView.Stretch)
@@ -39,6 +66,14 @@ class TableWidget(QWidget):
             # As a side effect, it also forces the header to resize to fit its container.
             self.tableView.verticalHeader().sectionSize(headerIndex)
         self.tableView.verticalHeader().setSectionResizeMode(QHeaderView.Interactive)
+
+    '''
+        Function: test
+        Parameters: self
+        Return Value: N/A
+        Purpose: Populates TableView with some dummy data to test if its properly displaying content.
+
+    '''
 
     def test(self):
         for i in range(8, 20):
